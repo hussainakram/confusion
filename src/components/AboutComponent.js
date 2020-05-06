f9a37f7efffdc99664f5773ef4ca5d333f5946c0
hussainakram <hussain.akram128@gmail.com>
2020-05-06 15:46:04 +0500

    controlled forms

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -22,7 +22,7 @@ const About = (props) => {
 
   const leaders = props.leaders.map((leader) => {
     return (
-      <RenderLeader leader={leader} />
+      <RenderLeader leader={leader} key={leader.id} />
     );
   });
 
